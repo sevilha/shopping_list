ef5b10896699df8d057f5743d179065813ed320a
Vinicius Sevilha <vd.sevilha@gmail.com>
2019-11-24 10:00:22 -0300

    Starting authentication coding

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './guards/auth.guard';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'lists',
+    pathMatch: 'full'
+  },
+  {
+    path: 'lists',
+    component: ListComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
