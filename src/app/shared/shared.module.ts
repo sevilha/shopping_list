321cf3d8622dc3adc0cb0e74b330714924274bd8
Vinicius Sevilha <vd.sevilha@gmail.com>
2020-01-28 06:30:47 -0300

    Create Friend Component
    Create friend component and field control error, add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FieldControlErrorComponent } from './field-control-error/field-control-error.component';
+
+@NgModule({
+  declarations: [FieldControlErrorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
